46403e07d37740f0ce11b162fb08a0ffe985164a
mark-ndonye <gitaundonye4@gmail.com>
2020-06-06 22:11:00 +0300

    update users registration

--- awards/urls.py
+++ awards/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('awards/', include('awardsapp.urls'))
+    path('awards/', include('awardsapp.urls')),
+    path('', include('users.urls'))
 ]
