62b77035f6cd762057e96375f352aa98fe626d95
mark-ndonye <gitaundonye4@gmail.com>
2020-06-07 00:32:28 +0300

    update urls.py

--- awards/urls.py
+++ awards/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,5 @@ urlpatterns = [
     path('login', LoginView.as_view(), name='login'),
     path('logout', LogoutView.as_view(template_name='registration/logout.html'), name='logout')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
