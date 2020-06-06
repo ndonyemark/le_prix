6d174cd607fa3bfe171311cfbfdb6cc52cb66284
mark-ndonye <gitaundonye4@gmail.com>
2020-06-06 20:31:26 +0300

    add hmepage view function

--- awardsapp/views.py
+++ awardsapp/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def homepage(request):
+
+    title='Homepage'
+    context={
+        'title':title
+    }
+    return render(request, 'index.html')
