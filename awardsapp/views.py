eb890b7bc285c408ccd2e66a025bb70716a51de6
mark-ndonye <gitaundonye4@gmail.com>
2020-06-07 00:22:48 +0300

    add post_project view

--- awardsapp/views.py
+++ awardsapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .forms import ProjectRegistrationForm
 
 def homepage(request):
 
@@ -9,7 +10,16 @@ def homepage(request):
     return render(request, 'index.html', context)
 
 def post_project(request):
-
+    current_user = request.user
+    if request.method == 'POST':
+        project_registration_form = ProjectRegistrationForm(request.POST)
+        if project_registration_form.is_valid():
+            project = project_registration_form.save(commit=False)
+            project.project_owner = current_user
+            project.save()
+            return redirect('post_project')
+    else:
+        project_registration_form = ProjectRegistrationForm()
     title = 'Post Project'
     context={
         'title':title,
