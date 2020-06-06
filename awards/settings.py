b1c53bc027b72125c72fe69cfb30dce0e961d0dd
mark-ndonye <gitaundonye4@gmail.com>
2020-06-07 00:29:29 +0300

    add a media settings in settings.py

--- awards/settings.py
+++ awards/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.templates.context_processors.media'
             ],
         },
     },
@@ -130,3 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = [
+    os.path.join(BASE_DIR, 'media/')
+]
