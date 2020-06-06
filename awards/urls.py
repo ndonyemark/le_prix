febd5120b9bfe2da266d4aa8870b99553873e072
mark-ndonye <gitaundonye4@gmail.com>
2020-06-06 23:15:41 +0300

    add login and logout views

--- awards/urls.py
+++ awards/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('awards/', include('awardsapp.urls')),
-    path('', include('users.urls'))
+    path('', include('users.urls')),
+    path('login', LoginView.as_view(), name='login'),
+    path('logout', LogoutView.as_view(template_name='registration/logout.html', name='logout'))
 ]
