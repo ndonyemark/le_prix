426dba0a416d5294b6bb1d6277569d1fd837f454
mark-ndonye <gitaundonye4@gmail.com>
2020-06-07 00:26:35 +0300

    add a post_project url

--- awardsapp/urls.py
+++ awardsapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage, name='homepage')
+    path('', views.homepage, name='homepage'),
+    path('post_project', views.post_project, name='post_project')
 ]
