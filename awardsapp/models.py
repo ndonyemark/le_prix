93f841b8aeab085efda14d532a58476d0d2379c6
mark-ndonye <gitaundonye4@gmail.com>
2020-06-07 00:10:25 +0300

    add Project model

--- awardsapp/models.py
+++ awardsapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Project(models.Model):
+    project_title = models.CharField(max_length=50)
+    project_image = models.ImageField(upload_to='project_pics')
+    project_description = models.TextField()
+    project_live_link = models.CharField(max_length=100)
+    project_pub_date = models.DateTimeField(auto_now_add=True)
+    project_owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.project_title
