de48e7f4a1bf3bc80d6bb2ea4007447082f0904b
mark-ndonye <gitaundonye4@gmail.com>
2020-06-06 23:00:14 +0300

    save registratin form details

--- users/views.py
+++ users/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
 
 def user_registration(request):
-
+    if request.method == 'POST':
+        registration_form = UserRegistrationForm(request.POST)
+        if registration_form.is_valid():
+            registration_form.save()
+            return redirect('login')
+    else:
+        registration_form = UserRegistrationForm()
     title='user_registration'
     context={
         'title': title,
