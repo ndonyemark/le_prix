8a242f234c195aa795d429b9b763d237e5721448
mark-ndonye <gitaundonye4@gmail.com>
2020-06-06 20:15:01 +0300

    register users, awardsapp, bootstrap and crispy forms

--- awards/settings.py
+++ awards/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'awardsapp',
+    'users',
+    'bootstrap4',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
