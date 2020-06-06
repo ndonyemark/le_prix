6e57cebd74782cf9dc6dae5b1ade72c8146aead9
mark-ndonye <gitaundonye4@gmail.com>
2020-06-07 00:18:53 +0300

    update forms.py file

---
+++ awardsapp/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Project
+
+class ProjectRegiatrtionForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = ['project_title', 'project_description', 'project_live_link', 'project_image']
