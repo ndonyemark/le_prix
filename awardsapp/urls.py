4e31b28c4fcf9d7e8125c3f57d3c0de40d109a59
mark-ndonye <gitaundonye4@gmail.com>
2020-06-07 14:49:42 +0300

    update single project url

--- awardsapp/urls.py
+++ awardsapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('post_project', views.post_project, name='post_project')
+    path('post_project', views.post_project, name='post_project'),
+    path('single_project/<int:project_id>', views.single_project, name='single_project')
 ]
