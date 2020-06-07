f6fdff3961f2564b8b92ce94d070e7102fc18ec9
mark-ndonye <gitaundonye4@gmail.com>
2020-06-07 16:04:54 +0300

    add a profile view function

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.user_registration, name='register')
+    path('register/', views.user_registration, name='register'),
+    path('profile/', views.profile, name='profile')
 ]
