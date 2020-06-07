1ef2734b76a5c559e9dc2c41f7346c14180f9f67
mark-ndonye <gitaundonye4@gmail.com>
2020-06-07 15:23:20 +0300

    add the search url

--- awardsapp/urls.py
+++ awardsapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('post_project', views.post_project, name='post_project'),
-    path('single_project/<int:project_id>', views.single_project, name='single_project')
+    path('single_project/<int:project_id>', views.single_project, name='single_project'),
+    path('search', views.search, name='search')
 ]
