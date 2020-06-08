10380640409c2d448c771fd59d21161d99e4e872
mark-ndonye <gitaundonye4@gmail.com>
2020-06-08 17:25:14 +0300

    add staticfiles

--- awards/settings.py
+++ awards/settings.py
@@ -135,8 +135,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
+    os.path.join(BASE_DIR, 'static')
 ] 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
